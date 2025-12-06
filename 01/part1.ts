3728edf2db55c30ae74ce78568e6f6c0cad03fba
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-06 13:05:41 +0100

    ✨ day1 part2

--- 01/part1.ts
+++ 01/part1.ts
@@ -1,7 +1,41 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./01/input.txt").toString();
+const input = fs.readFileSync("./01/input2.txt").toString();
 
 const lines = input.split("\n");
 
-console.log(lines);
+const initialPosition = 50;
+
+let direction = '';
+let actualPosition = initialPosition;
+let rotationNumber = 0;
+
+
+const executeInstruction = (lines: string[]) => {
+    let zeroCount = 0;
+    for(const line of lines) {
+        direction = line.charAt(0);
+        rotationNumber = Number(line.slice(1));
+        if(direction === 'L') {
+            const res = actualPosition - rotationNumber;
+            if(res < 0) {
+                actualPosition = (100 - Math.abs(res) % 100) % 100;
+            } else {
+                actualPosition = res;
+            }
+        } else {
+            const res = actualPosition + rotationNumber;
+            actualPosition = res % 100;
+        }
+        console.log('the dial rotated ' + line);
+        
+        console.log('actual position ' + actualPosition);
+        if (actualPosition === 0) {
+            zeroCount++;
+        }
+    }
+    return zeroCount;
+}
+
+console.log(executeInstruction(lines));
+
--- 01/part2.ts
+++ 01/part2.ts
@@ -1,7 +1,40 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./01/input.txt").toString();
+const input = fs.readFileSync("./01/input2.txt").toString();
 
 const lines = input.split("\n");
 
-console.log(lines);
+const initialPosition = 50;
+
+let direction = '';
+let actualPosition = initialPosition;
+let prevPos = actualPosition;
+let rotationNumber = 0;
+
+const executeInstruction = (lines: string[]) => {
+	let zeroCount = 0;
+	for(const line of lines) {
+		direction = line.charAt(0);
+		rotationNumber = Number(line.slice(1));
+		prevPos = actualPosition;
+		if(direction === 'L') {
+			const res = actualPosition - rotationNumber;
+			if(res <= 0) {
+				if(prevPos !== 0)
+					zeroCount++;
+				zeroCount += Math.floor(Math.abs(res) / 100);
+				actualPosition = (100 - Math.abs(res) % 100) % 100;
+			} else {
+				actualPosition = res;
+			}
+		} else {
+			const res = actualPosition + rotationNumber;           
+			if(res >= 100)
+				zeroCount += Math.floor(res / 100);
+			actualPosition = res % 100;
+		}
+	}
+	return zeroCount;
+}
+
+console.log(executeInstruction(lines));
