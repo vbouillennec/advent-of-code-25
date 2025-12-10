137ebc8d853c99e608a9fce87b84d2831ac5e752
Valentin <val.bouillennec@gmail.com>
2025-12-11 00:43:03 +0100

    ✨ day6 part1

---
+++ 06/part2.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./06/input.txt").toString();
+
+const problemsParts = input.split("\n").map((row) => row.match(/\S+/g));
+
+const problemsOperations = problemsParts.pop();
+
+function makeCalculation(problemsParts: RegExpMatchArray[], problemsOperations: string[]) {
+	let total = 0;
+	for(let i = 0; i < problemsOperations.length; i++) {
+		const problemSymbol = problemsOperations[i];
+		let result = problemSymbol === '*' ? 1 : 0;
+		for(let j = 0; j < problemsParts.length; j++) {
+			if(problemSymbol === '*') {
+				result *= Number(problemsParts[j][i]);
+			} 
+			else if(problemSymbol === '+') {
+				result += Number(problemsParts[j][i]);
+			}
+		}
+		total += result; 
+	}
+	return total;
+}
+
+console.log(makeCalculation(problemsParts, problemsOperations));
