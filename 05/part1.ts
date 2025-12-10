d4061cca8c1a18f218abcc4a1027211c826101e9
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-10 17:46:22 +0100

    ✨ day5 part1

--- 05/part1.ts
+++ 05/part1.ts
@@ -1,3 +1,31 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./05/input.txt").toString().split("\r\n\r\n");
+const [ingredientIDsRangesPart, availableIngredientsPart] = fs.readFileSync("./05/input2.txt").toString().split("\n\n");
+
+const ingredientIDsRanges = ingredientIDsRangesPart.split("\n").map((line) => {
+    const [start, end] = line.split("-");
+    return { start: Number(start), end: Number(end) };
+});
+const availableIngredients = availableIngredientsPart.split("\n").map(ingredient => Number(ingredient));
+
+function checkAllIngredientsFreshness(ingredients: number[]): number {
+    let freshIngredients = 0;
+    for (const ingredient of ingredients) {
+        if (checkIngredientFreshness(ingredient))
+            freshIngredients++;
+    }
+    return freshIngredients;
+}
+function checkIngredientFreshness(ingredient: number) {
+    for (const ingredientIDsRange of ingredientIDsRanges) {
+        const rangeStart = ingredientIDsRange.start;
+        const rangeEnd = ingredientIDsRange.end;
+        if (ingredient >= rangeStart && ingredient <= rangeEnd)
+            return true;
+    }
+    return false;
+}
+
+const nbOfFreshIngredients = checkAllIngredientsFreshness(availableIngredients);
+
+console.log(nbOfFreshIngredients);
