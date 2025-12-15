72bea59595085a96a115c53d0b86b486ea0d069c
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-15 17:31:27 +0100

    day6 part2

--- 06/part2.ts
+++ 06/part2.ts
@@ -1,27 +1,65 @@
 import fs from "fs";
 
-const input = fs.readFileSync("./06/input.txt").toString();
+const input = fs.readFileSync("./06/input2.txt").toString();
 
-const problemsParts = input.split("\n").map((row) => row.match(/\S+/g));
+const operations2D = input.split("\n").map((line) => line.split(''));
 
-const problemsOperations = problemsParts.pop();
+const symbolLine = operations2D.pop() as string[];
 
-function makeCalculation(problemsParts: RegExpMatchArray[], problemsOperations: string[]) {
+function makeCalculation(symbolLine: string[]) {
 	let total = 0;
-	for(let i = 0; i < problemsOperations.length; i++) {
-		const problemSymbol = problemsOperations[i];
-		let result = problemSymbol === '*' ? 1 : 0;
-		for(let j = 0; j < problemsParts.length; j++) {
-			if(problemSymbol === '*') {
-				result *= Number(problemsParts[j][i]);
-			} 
-			else if(problemSymbol === '+') {
-				result += Number(problemsParts[j][i]);
+	let problemStartIndex = 0;
+	let problemSymbol = symbolLine[problemStartIndex];
+	let problemEndIndex = 0;
+	for (let i = 0; i < symbolLine.length; i++) {
+		if ((symbolLine[i] === '*' || symbolLine[i] === '+' || i === symbolLine.length - 1) && problemStartIndex !== i) {
+			if (i === symbolLine.length - 1) {
+				problemEndIndex = i;
+			} else {
+				problemEndIndex = i - 2;
+			}
+			if (problemSymbol === '+') {
+				total += sumResolve(problemStartIndex, problemEndIndex);
+			} else if (problemSymbol === '*') {
+				total += productResolve(problemStartIndex, problemEndIndex);
+			}
+			if (i !== symbolLine.length - 1) {
+				problemSymbol = symbolLine[i];
+				problemStartIndex = i;
 			}
 		}
-		total += result; 
 	}
 	return total;
 }
 
-console.log(makeCalculation(problemsParts, problemsOperations));
+function sumResolve(start: number, end: number) {
+	let result = 0;
+	let strNumber = '';
+	let colIndex = end;
+	while (colIndex >= start) {
+		for (let lineIndex = 0; lineIndex < operations2D.length; lineIndex++) {
+			strNumber = strNumber + operations2D[lineIndex][colIndex];
+		}
+		colIndex--;
+		result += Number(strNumber);
+		strNumber = '';
+	}
+	return result;
+}
+
+function productResolve(start: number, end: number) {
+	let result = 1;
+	let strNumber = '';
+	let colIndex = end;
+	while (colIndex >= start) {
+		for (let lineIndex = 0; lineIndex < operations2D.length; lineIndex++) {
+			strNumber = strNumber + operations2D[lineIndex][colIndex];
+		}
+		colIndex--;
+		result *= Number(strNumber);
+		strNumber = '';
+	}
+	return result;
+}
+
+console.log(makeCalculation(symbolLine));
