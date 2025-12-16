c1046c0eecd8bd6c75aaa5729c64c49a6d1f8719
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-16 18:00:09 +0100

    🚧 [WIP] day7 part2

--- 07/part2.ts
+++ 07/part2.ts
@@ -1,14 +1,14 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('./07/input.txt').toString();
+const input = fs.readFileSync('./07/input2.txt').toString();
 
 const map2D = input.split('\n').map(line => line.split(''));
 
 let nbOfTimelines = 0;
+const startPos = map2D[0].indexOf('S');
+const tachyonBeams = new Set([startPos]);
 
 function drawTachyonBeam() {
-	const startPos = map2D[0].indexOf('S');
-	const tachyonBeams = new Set([startPos]);
 	let splitCount = 0;
 	for (let i = 1; i < map2D.length; i++) {
 		for (const tachyonBeam of tachyonBeams) {
@@ -31,32 +31,30 @@ function drawTachyonBeam() {
 const splitCount = drawTachyonBeam();
 
 function countTimelines() {
-	const lastLineIndex = map2D.length - 1
-	for(let j = map2D.length - 1; j >= 0; j--) {
-		if(map2D[lastLineIndex][j] === '|') {
-			backToSource(lastLineIndex, j);
+	const lastLineIndex = 1;
+	for (let i = 0; i < map2D[lastLineIndex].length; i++) {
+		if (map2D[lastLineIndex][i] === '|') {
+			console.log(`starting backtrack from li: ${lastLineIndex}, ci: ${i}, ${tachyonBeams.size} beams`);
+			tryATimeline(lastLineIndex, i);
 		}
 	}
 }
 
-function backToSource(lineIndex, colIndex) {
-	const li = lineIndex - 1;
+function tryATimeline(lineIndex: number, colIndex: number) {
+	const li = lineIndex + 1;
 	const ci = colIndex;
 	if (li === 0 && map2D[li][ci] === 'S') {
 		nbOfTimelines++;
 		return;
 	}
-	if(map2D[li][ci] === '|') {
-		// console.log(`back to source - li: ${li}, ci: ${ci}`);
-		backToSource(li, ci);
+	if (map2D[li][ci] === '|') {
+		tryATimeline(li, ci);
 	}
-	if(map2D[li][ci + 1] === '|') {
-		// console.log(`back to source - li: ${li}, ci: ${ci+1}`);
-		backToSource(li, ci + 1);
+	if (map2D[li + 1][ci + 1] === '^' && map2D[li][ci + 1] === '|') {
+		tryATimeline(li, ci + 1);
 	}
-	if(map2D[li][ci - 1] === '|') {
-		// console.log(`back to source - li: ${li}, ci: ${ci-1}`);
-		backToSource(li, ci - 1);
+	if (map2D[li + 1][ci - 1] === '^' && map2D[li][ci - 1] === '|') {
+		tryATimeline(li, ci - 1);
 	}
 }
 
@@ -70,6 +68,6 @@ console.log(splitCount);
 
 printMap(map2D);
 
-// countTimelines();
+countTimelines();
 
-// console.log('timelines: ', nbOfTimelines);
+console.log('timelines: ', nbOfTimelines);
