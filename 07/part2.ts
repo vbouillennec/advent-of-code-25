3a0b43d825fa2edf655b7af378815bf787e0920c
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-17 11:28:42 +0100

    ✨ day7 part2

--- 07/part2.ts
+++ 07/part2.ts
@@ -1,75 +1,38 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('./07/input.txt').toString();
+const input = fs.readFileSync('./07/input2.txt').toString();
 
 const map2D = input.split('\n').map(line => line.split(''));
 
-let nbOfTimelines = 0;
 const startPos = map2D[0].indexOf('S');
-const tachyonBeams = new Set([startPos]);
-const paths = new Map<string, number[]>();
-
-function drawTachyonBeam() {
-	let splitCount = 0;
-	for (let i = 1; i < map2D.length; i++) {
-		for (const tachyonBeam of tachyonBeams) {
-			if (map2D[i][tachyonBeam] === '.') {
-				tachyonBeams.add(tachyonBeam);
-				map2D[i][tachyonBeam] = '|';
-			} else if (map2D[i][tachyonBeam] === '^') {
-				splitCount++;
-				tachyonBeams.delete(tachyonBeam);
-				tachyonBeams.add(tachyonBeam - 1);
-				tachyonBeams.add(tachyonBeam + 1);
-				map2D[i + 1][tachyonBeam - 1] = '|';
-				map2D[i + 1][tachyonBeam + 1] = '|';
-			}
-		}
-	}
-	return splitCount;
-}
-
-const splitCount = drawTachyonBeam();
+const splits = new Map<string, number>();
 
 function countTimelines() {
-	const lastLineIndex = 1;
-	for (let i = 0; i < map2D[lastLineIndex].length; i++) {
-		if (map2D[lastLineIndex][i] === '|') {
-			const potentialPath = new Map();
-			tryATimeline(lastLineIndex, i, potentialPath);
-		}
+	const firstLineIndex = 1;
+	for (let i = 0; i < map2D[firstLineIndex].length; i++) {
+		return tryATimeline(firstLineIndex, startPos);
 	}
 }
 
-function tryATimeline(lineIndex: number, colIndex: number, potentialPath: Map<string, number[]>) {
+function tryATimeline(lineIndex: number, colIndex: number) {
 	const li = lineIndex + 1;
 	const ci = colIndex;
-	potentialPath.set(`[${li},${ci}]`, [li,ci]);
-	if (li === (map2D.length - 1) && map2D[li][ci] === '|') {
-		nbOfTimelines++;
-		return;
+	if (li === (map2D.length - 1)) {
+		return 1;
 	}
-	if (map2D[li][ci] === '|') {
-		tryATimeline(li, ci);
+	if (map2D[li][ci] === '^') {
+		if (splits.has(`[${li},${ci}]`)) {
+			return splits.get(`[${li},${ci}]`)!;
+		}
+		const rightTimeline = tryATimeline(li, ci + 1);
+		const leftTimeline = tryATimeline(li, ci - 1);
+		splits.set(`[${li},${ci}]`, (leftTimeline + rightTimeline));
+		return leftTimeline + rightTimeline;
 	}
-	if (map2D[li][ci] === '^' && map2D[li][ci + 1] === '|') {
-		tryATimeline(li, ci + 1);
-	}
-	if (map2D[li][ci] === '^' && map2D[li][ci - 1] === '|') {
-		tryATimeline(li, ci - 1);
-	}
+	const straightTimeline = tryATimeline(li, ci);
+	return straightTimeline;
 }
 
-function printMap(map2D: string[][]) {
-	for(let i = 0; i < map2D.length; i++) {
-		console.log(map2D[i].join(''));
-	}
-}
-
-console.log(splitCount);
-
-printMap(map2D);
-
-countTimelines();
+const nbOfTimelines = countTimelines();
 
 console.log('timelines: ', nbOfTimelines);
