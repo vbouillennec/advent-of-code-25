5afd93353b83b6363dd99e1d6dd08aa42cea5f62
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-17 18:39:24 +0100

    ✨ day9 part1

--- 09/part1.ts
+++ 09/part1.ts
@@ -1,3 +1,27 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('./09/input.txt').toString();
+const input = fs.readFileSync('./09/input2.txt').toString();
+
+const lines = input.split('\n').map(line => {
+    const [y, x] = line.split(',');
+    return { x: Number(x), y: Number(y) };
+});
+
+function calcArea(line: { x: number, y: number }, otherLine: { x: number, y: number }): number {
+    const dx = Math.abs(line.x - otherLine.x) + 1;
+    const dy = Math.abs(line.y - otherLine.y) + 1;
+    const area = dx * dy;
+    return area;
+}
+
+let largestArea = 0;
+
+for (const line of lines) {
+    for (const otherLine of lines) {
+        if (line === otherLine) continue;
+        const areaSize = calcArea(line, otherLine);
+        largestArea = Math.max(largestArea, areaSize);
+    }
+}
+
+console.log('largest area is: ', largestArea);
