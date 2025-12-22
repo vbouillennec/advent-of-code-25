2f6617c2e96b155888f31730e49e8098e5e17a6c
vbouillennec <vbouillennec@financesetcreation.fr>
2025-12-22 15:00:29 +0100

    ✨ day11 part1

--- 11/part1.ts
+++ 11/part1.ts
@@ -1,3 +1,31 @@
 import fs from 'fs';
 
-const input = fs.readFileSync('./11/input.txt').toString();
+const lines = fs.readFileSync('./11/input2.txt').toString().split('\n');
+
+const devicesMap = new Map<string, string[]>();
+
+for (const line of lines) {
+    const [name, outputs] = line.split(': ')
+    devicesMap.set(name, outputs.split(' '))
+}
+
+const nbOfPaths = findAllPaths(devicesMap);
+
+console.log(`il y a ${nbOfPaths} chemins`);
+
+/* Functions */
+
+function findAllPaths(devices: Map<string, string[]>): number {
+    return findPath('you', devices)
+}
+
+function findPath(input: string, devices: Map<string, string[]>, paths = 0): number {
+    if (input === 'out') {
+        return ++paths;
+    }
+    const outputs = devices.get(input);
+    for (const output of outputs) {
+        paths = findPath(output, devices, paths);
+    }
+    return paths;
+}
